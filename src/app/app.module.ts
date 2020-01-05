03f837039672c1321fd33cb60814afcf2d778d2d
Vinoda Patil <vspatil678@gmail.com>
2020-01-05 11:55:27 +0530

    packages added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,39 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { ResumeBuilderComponent } from './resume-builder/resume-builder.component';
+import { ResumeBuilderService } from './services/resume-builder.service';
+import { ResumeValidationComponent } from './resume-validation/resume-validation.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { NgxKjuaModule } from 'ngx-kjua';
+import { MatDialogModule } from '@angular/material/dialog';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResumeBuilderComponent,
+    ResumeValidationComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatIconModule,
+    MatToolbarModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgxKjuaModule,
+    MatDialogModule
   ],
-  providers: [],
+  providers: [
+    ResumeBuilderService
+  ],
+  entryComponents: [
+    ResumeValidationComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
